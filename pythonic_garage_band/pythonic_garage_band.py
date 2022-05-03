3164e1535057bbb7cb4d5a9abd742c161a483ba0
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-02 21:43:36 -0700

    all tests passing

--- pythonic_garage_band/pythonic_garage_band.py
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -1,12 +1,28 @@
 class Band:
-    pass
+    instances = []
+
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+        Band.instances.append(self)
+
+    def play_solos(self):
+        solos = []
+        for member in self.members:
+            solos.append(member.play_solo())
+        return tuple(solos)
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
 
 
 class Musician():
-    pass
+    def __init__(self, name='unknown', instrument='unknown'):
+        self.name = name
 
 
-class Guitarist():
+class Guitarist(Musician):
     def __init__(self, name):
         self.name = name
 
@@ -16,12 +32,35 @@ class Guitarist():
     def __repr__(self):
         return f"Guitarist instance. Name = {self.name}"
 
+    @staticmethod
+    def play_solo():
+        return "face melting guitar solo"
 
-class Bassist():
-    pass
+    @classmethod
+    def get_instrument(cls):
+        return 'guitar'
 
 
-class Drummer():
+class Bassist(Musician):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
+    @staticmethod
+    def play_solo():
+        return "bom bom buh bom"
+
+    @classmethod
+    def get_instrument(cls):
+        return 'bass'
+
+
+class Drummer(Musician):
     # were creating a new instantiation of drummer class with property name
     def __init__(self, name):
         # assigning a property =
@@ -32,3 +71,11 @@ class Drummer():
 
     def __repr__(self):
         return f"Drummer instance. Name = {self.name}"
+
+    @staticmethod
+    def play_solo():
+        return "rattle boom crash"
+
+    @classmethod
+    def get_instrument(cls):
+        return 'drums'
